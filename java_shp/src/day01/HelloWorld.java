2fcac2eba1e6a6249e5646cdaac04990ba41ea5b
adsihn <153248298+adsihn@users.noreply.github.com>
2023-12-08 17:16:43 +0900

    자바 1일차; Hello world

--- java_shp/src/day01/HelloWorld.java
+++ java_shp/src/day01/HelloWorld.java
@@ -1,10 +1,22 @@
 package day01;
 
 public class HelloWorld {
+	
+	//주석1
+	
+	/*
+	 * 주석2
+	 */
+	
+	/**
+	 * 메소드를 설명할 때 사용하는 주석
+	 * @param args
+	 */
 
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world!");
+		//sysout은 콘솔 출력할 때 사용
 	}
 
 }
