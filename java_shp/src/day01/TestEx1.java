f5ae2bf36f2b68471ab0a1dda7ce9d306e515d96
adsihn <153248298+adsihn@users.noreply.github.com>
2023-12-08 17:40:49 +0900

    자바 1일차; 문제

---
+++ java_shp/src/day01/TestEx1.java
@@ -0,0 +1,9 @@
+package day01;
+
+public class TestEx1 {
+	//아래 코드에서 잘못된 부분을 찾아 수정하세요.
+	//char ch1 = "A";
+	public static void main(String[] args) {
+		char ch1 = 'A';
+	}
+}
---
+++ java_shp/src/day01/TestEx2.java
@@ -0,0 +1,10 @@
+package day01;
+
+public class TestEx2 {
+	//국어, 영어, 수학 성적을 입력하여 평균을 구하려 한다. 이때 필요한 변수들을 선언하세요.
+	public static void main(String[] args) {
+		int kor1, eng1, math1, sum1; //점수가 정수일 경우.
+		double kor2, eng2, math2, sum2; //점수가 소수점일 경우.
+		double avg; //평균은 실수일 수 있다.
+	}
+}
