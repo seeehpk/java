a2307f8528cb657e3f674c851d1ba50edc21a0a6
adsihn <153248298+adsihn@users.noreply.github.com>
2023-12-11 17:49:24 +0900

    자바 2일차; 조건문

---
+++ java_shp/src/day02/IfMultiple.java
@@ -0,0 +1,21 @@
+package day02;
+
+import java.util.Scanner;
+
+public class IfMultiple {
+
+	public static void main(String[] args) {
+		//정수를 입력받아 3의 배수인지 아닌지 판별하는 코드를 작성하세요.
+		Scanner scan = new Scanner(System.in);
+		System.out.print("정수를 입력 : ");
+		int num = scan.nextInt();
+		
+		if(num % 3 == 0) {
+			System.out.println("3의 배수입니다.");
+		}else {
+			System.out.println("3의 배수가 아닙니다.");
+		}
+		
+		scan.close();
+	}
+}
